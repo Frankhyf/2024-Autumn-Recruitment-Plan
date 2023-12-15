4c6040f5e123dda49fb575ad434d211950f38276
一帆 黄 <529048134@qq.com>
2023-12-16 02:13:11 +0800

    12/16任务全部完成

--- 数据结构与算法/LinearSearch/src/LinearSearch.java
+++ 数据结构与算法/LinearSearch/src/LinearSearch.java
@@ -1,10 +1,10 @@
 public class LinearSearch {
    // public int  search(int[] data,int target){ 不需要每次创建一个新的对象
     private  LinearSearch(){}//让linearsearch的构造函数是私有的，不允许用户使用构造函数动态创建对象的方式
-   public static int  search(int[] data,int target){ //直接调用固定的
+   public static  <E> int search(E[] data,E target){ //使用泛型数据
 
        for (int i = 0 ;i< data.length;i++)
-            if(data[i] == target)
+            if(data[i].equals(target))//判断两个类对象相等
                 return i; //只有一行的话大括号可以省略，
 
         return -1;
--- 数据结构与算法/LinearSearch/src/Main.java
+++ 数据结构与算法/LinearSearch/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        int[] data = {24,1,16,453,65,786,8675,432};
+        Integer[] data = {24,1,16,453,65,786,8675,432};//int基本数据类型的包装类Integer
 
         //LinearSearch ls = new LinearSearch();
         int res = LinearSearch.search(data, 16);//这里不能直接输入target而是直接输入16，target让系统自动生成
